e156cb785a44f77ce7a88582c88aa0accec2cbfb
Rolwane Borges <rolwane@hotmail.com>
2021-11-11 19:06:47 -0300

    Exercicios 01 e 02

---
+++ fundamentos/Bloco-07-Introducao-a-JavaScript-ES6-e-Testes-Unitarios/Dia-02-JavaScript-ES6-Objects/Parte I/Exercise-02/script.js
@@ -0,0 +1,48 @@
+const order = {
+  name: 'Rafael Andrade',
+  phoneNumber: '11-98763-1416',
+  address: {
+    street: 'Rua das Flores',
+    number: '389',
+    apartment: '701',
+  },
+  order: {
+    pizza: {
+      marguerita: {
+        amount: 1,
+        price: 25,
+      },
+      pepperoni: {
+        amount: 1,
+        price: 20,
+      }
+    },
+    drinks: {
+      coke: {
+        type: 'Coca-Cola Zero',
+        price: 10,
+        amount: 1,
+      }
+    },
+    delivery: {
+      deliveryPerson: 'Ana Silveira',
+      price: 5,
+    }
+  },
+  payment: {
+    total: 60,
+  },
+};
+
+// 2. Complete a função orderModifier() para que seu retorno seja similar a "Olá Luiz Silva, o total do seu
+// pedido de marguerita, pepperoni e Coca-Cola Zero é R$ 50,00."
+
+const orderModifier = (order) => {
+  order.order.delivery.deliveryPerson = 'Luiz Silva';
+  order.payment.total = 50;
+
+  console.log(`Olá ${order.order.delivery.deliveryPerson}, o total do seu pedido de ${Object.keys(order.order.pizza)[0]}, ${Object.keys(order.order.pizza)[1]} e ${order.order.drinks.coke.type} é R$ ${order.payment.total},00.`)
+
+}
+
+orderModifier(order);
