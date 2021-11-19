13b662e45150f39928daa76de0517023165b2336
Rolwane Borges <rolwane@hotmail.com>
2021-11-19 15:28:54 -0300

    Exercise 03

---
+++ fundamentos/Bloco-08-Higher-Order-Functions-do-JavaScript-ES6/Dia-01-Introducao-a-Higher-Order-Functions/Exercise-03/script.js
@@ -0,0 +1,29 @@
+// 3. Crie uma HOF que receberá três parâmetros. O primeiro será um array de respostas corretas (Gabarito),
+// o segundo será um array de respostas a serem verificadas (respostas da pessoa estudante) e o terceiro é uma
+// função que checa se as respostas estão corretas e faz a contagem da pontuação final recebida pela pessoa
+// estudante. Ao final a HOF deve retornar o total da contagem de respostas certas.
+
+
+// Quando a resposta for correta a contagem sobe 1 ponto, quando for incorreta desce 0.5 pontos,
+// e quando não houver resposta ("N.A") não altera-se a contagem.
+
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+
+const checkAnswers = (RIGHT_ANSWERS, STUDENT_ANSWERS) => {
+
+  let nota = 0;
+
+  STUDENT_ANSWERS.forEach((studentAnswer, i) => {
+    nota += studentAnswer === RIGHT_ANSWERS[i] ? 1 : studentAnswer === 'N.A' ? 0 : -0.5;
+  });
+
+  return nota;
+}
+
+const nota = (RIGHT_ANSWERS, STUDENT_ANSWERS, callback) => {
+  return `A nota do estudante foi: ${callback(RIGHT_ANSWERS, STUDENT_ANSWERS)}`;
+}
+
+console.log(nota(RIGHT_ANSWERS, STUDENT_ANSWERS, checkAnswers));
