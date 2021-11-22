ad1f05006a0e022669475c5754596589b73bf3a4
Rolwane Borges <rolwane@hotmail.com>
2021-11-22 19:01:34 -0300

    Exercise 03 - Crie um array com todos os objetos que possuem gênero ficção científica ou fantasia.

---
+++ fundamentos/Bloco-08-Higher-Order-Functions-do-JavaScript-ES6/Dia-03-map-e-filter/Exercise-03/script.js
@@ -0,0 +1,11 @@
+const books = require('../books');
+
+
+// 3. Crie um array com todos os objetos que possuem gênero ficção científica ou fantasia.
+
+function fantasyOrScienceFiction(books) {
+  return books.filter((book) => book.genre === 'Fantasia' || book.genre === 'Ficção Científica');
+}
+
+
+console.log(fantasyOrScienceFiction(books));
