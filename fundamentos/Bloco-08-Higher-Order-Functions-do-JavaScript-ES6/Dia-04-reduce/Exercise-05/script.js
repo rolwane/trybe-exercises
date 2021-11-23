41c93cd692c04b749af339c7a6528436c0171771
Rolwane Borges <rolwane@hotmail.com>
2021-11-23 18:43:31 -0300

    Exercise 05 - Dada o array de nomes, retorne a quantidade de vezes em que aparecem a letra a maiúscula ou minúscula.

---
+++ fundamentos/Bloco-08-Higher-Order-Functions-do-JavaScript-ES6/Dia-04-reduce/Exercise-05/script.js
@@ -0,0 +1,14 @@
+
+// 5. Dada o array de nomes, retorne a quantidade de vezes em que aparecem a letra a maiúscula ou minúscula.
+
+const names = [
+  'Aanemarie', 'Adervandes', 'Akifusa',
+  'Abegildo', 'Adicellia', 'Aladonata',
+  'Abeladerco', 'Adieidy', 'Alarucha',
+];
+
+function containsA(names) {
+  return names.reduce((acc, name) => acc + name.match(/A/gi).length, 0);
+}
+
+containsA(names);
