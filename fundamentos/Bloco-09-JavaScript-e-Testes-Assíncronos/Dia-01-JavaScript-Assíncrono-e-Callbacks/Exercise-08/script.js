c3e655c068de0dd55c772c1f5c16628df2d49c4e
Rolwane Borges <rolwane@hotmail.com>
2021-11-30 14:08:31 -0300

    Exercise 08

---
+++ fundamentos/Bloco-09-JavaScript-e-Testes-Assíncronos/Dia-01-JavaScript-Assíncrono-e-Callbacks/Exercise-08/script.js
@@ -0,0 +1,34 @@
+const pokemons = [
+  { name: 'Bulbasaur', type: 'Grass', ability: 'Razor Leaf' },
+  { name: 'Charmander', type: 'Fire', ability: 'Ember' },
+  { name: 'Squirtle', type: 'Water', ability: 'Water Gun' },
+];
+
+function showResult(error, message) {
+  if (message === null) {
+    console.log(error);
+  } else {
+    console.log(message);
+  }
+}
+
+function getPokemonDetails(filter, callback) {
+  setTimeout(() => {
+    if (pokemons.find(filter) === undefined) {
+      return callback(new Error('Não temos esse pokémon para você :('), null);
+    }
+    const pokemon = pokemons.find(filter);
+
+    const { name, type, ability } = pokemon;
+
+    const messageFromProfOak = `Olá, seu pokémon é o ${name}, o tipo dele é ${type} e a habilidade principal dele é ${ability}`;
+
+    callback(null, messageFromProfOak);
+  }, 2000);
+}
+
+getPokemonDetails((pokemon) => pokemon.name === 'Squirtle', showResult);
+
+module.exports = {
+  getPokemonDetails,
+};
